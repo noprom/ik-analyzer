6a8fa69524772d050a6d51e2cb9d1344e4691c28
noprom <tyee.noprom@qq.com>
2016-07-18 21:53:08 +0800

    add apps data

---
+++ src/main/java/com/huntdreams/ik/AppsAnalyzer.java
@@ -0,0 +1,18 @@
+package com.huntdreams.ik;
+
+/**
+ * AppsAnalyzer
+ * app应用简介以及分词
+ * <p/>
+ * Author: Noprom <tyee.noprom@qq.com>
+ * Date: 7/18/16 9:48 PM.
+ */
+public class AppsAnalyzer {
+
+    // 数据目录
+    private String dataPath = "data/app";
+
+    public static void main(String[] args) {
+
+    }
+}
